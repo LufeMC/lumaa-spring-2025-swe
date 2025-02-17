a7c1dde64f2a25f6be22d8504940de401245d1db
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-17 14:12:41 -0800

    starter homepage

---
+++ frontend/src/Components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Task Management App</h1>
+      <button>Register</button>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Home;
