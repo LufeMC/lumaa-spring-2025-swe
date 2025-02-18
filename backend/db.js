c7da9762065878e9e1bbbdb160781a4a916f0de0
Vishrutha1909 <vishruthar19@gmail.com>
2025-02-18 18:50:00 -0500

    order folder structure

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "TaskManagement",
-    password: "Vish@1909",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
-db.connect();
+db.connect()
+    .then(() => console.log("Connected to PostgreSQL"))
+    .catch(err => console.error("Connection error", err));
 
 export default db;
