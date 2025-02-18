a8dee5ecc66d2d796e1eb3cb34c3b7feb533a026
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-17 18:57:55 -0800

    troubleshooting / fixing routes

--- backend/server.js
+++ backend/server.js
@@ -2,36 +2,39 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 const { Pool } = require('pg');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
 const app = express();
+
+// import routes 
+const regRoutes = require('./routes/registration');
+//const taskRoutes = require('./routes/tasks');
+
 
 // middleware
 app.use(cors()); // cors
 app.use(bodyParser.json()); // to parse json res 
 
-console.log('Path to .env:', path.resolve(__dirname, '../.env'));
 //verify connection to the db 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-console.log('db url', process.env.DB_USER);
 pool.connect()
   .then(() => console.log('Connected to the database!'))
   .catch((err) => console.error('Database connection error:', err));
 
-/*
-- for when i write the routes 
-// import routes 
-const authRoutes = require('./routes/auth');
-const taskRoutes = require('./routes/tasks');
+
 
 // routes 
-app.use('/auth', authRoutes);
-app.use('/tasks', taskRoutes);
-*/
+app.use('/api/register', regRoutes);
+//app.use('/tasks', taskRoutes);
+
+// Add this AFTER all other routes
+app.use('*', (req, res) => {
+  console.log('Unhandled route:', req.originalUrl);
+  res.status(404).json({ error: 'Route not found' });
+});
+
 
 // start the server 
 const PORT = process.env.PORT || 3001;
