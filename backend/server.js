7792a9519205ba6ec385a6e5c318f255a67bd515
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-18 17:04:31 -0800

    reconfiguring middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
+const sequelize = require('./database/config/database');
+const authMiddleware = require('./authMiddleware');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const app = express();
 
 // import routes 
-const regRoutes = require('./routes/registration');
-//const taskRoutes = require('./routes/tasks');
+const regRoutes = require('./routes/auth/registration');
+const loginRoutes = require('./routes/auth/login');
+const taskRoutes = require('./routes/tasks/tasks');
 
 
 // middleware
 app.use(cors()); // cors
-app.use(bodyParser.json()); // to parse json res 
+app.use(express.json()); 
 
 //verify connection to the db 
 const pool = new Pool({
@@ -26,14 +29,25 @@ pool.connect()
 
 
 // routes 
-app.use('/api/register', regRoutes);
-//app.use('/tasks', taskRoutes);
+app.use('/api/auth/register', regRoutes);
+app.use('/api/auth/login', loginRoutes);
+app.use('/api/tasks', authMiddleware, taskRoutes);
 
 // Add this AFTER all other routes
 app.use('*', (req, res) => {
   console.log('Unhandled route:', req.originalUrl);
   res.status(404).json({ error: 'Route not found' });
 });
+
+// Database connection test
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
 // start the server 
