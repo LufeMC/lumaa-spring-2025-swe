9e4ba21e420b14b0e870c97230fc1f6fa6b6cc87
turisouvenir <turinumugishasouvenir@gmail.com>
2025-02-21 12:39:17 +0200

    Setup Axios, Zustand, and auth Store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/auth";
+import { useEffect } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+export default function App() {
+  const { fetchUser, user } = useAuthStore();
 
-function App() {
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
-    <div>Task Manager</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/dashboard" /> : <Register />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
